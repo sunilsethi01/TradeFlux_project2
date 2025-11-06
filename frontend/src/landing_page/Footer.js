72cba5f276fba414a404d98905b3f86ee1d7fe55
sunil sethi <sunilsethi1214@gmail.com>
2025-11-06 20:43:16 +0530

    Fix: ESLint errors - replace empty hrefs in Footer and remove unused navigate in Login

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -11,41 +11,41 @@ function Footer() {
                     </div>
                     <div className='footer-col'>
                         <p>Company</p>
-                        <a href=''>About</a> 
-                        <a href=''>Philosophy</a> 
-                        <a href=''>Press & media</a>
-                        <a href=''>Careers</a>
-                        <a href=''>TradeFlux Cares (CSR)</a> 
-                        <a href=''>TradeFlux.tech</a> 
-                        <a href=''>Open source</a>  
+                        <a href='#'>About</a> 
+                        <a href='#'>Philosophy</a> 
+                        <a href='#'>Press & media</a>
+                        <a href='#'>Careers</a>
+                        <a href='#'>TradeFlux Cares (CSR)</a> 
+                        <a href='#'>TradeFlux.tech</a> 
+                        <a href='#'>Open source</a>  
                     </div>
                     <div className='footer-col'>
                         <p>Quick links</p>
-                        <a href=''>Upcoming IPOs</a>
-                        <a href=''>Brokerage charges</a> 
-                        <a href=''>Market holidays</a>
-                        <a href=''>Economic calendar</a>
-                        <a href=''>Calculators</a> 
-                        <a href=''>Markets</a> 
-                        <a href=''>Sectors</a> 
+                        <a href='#'>Upcoming IPOs</a>
+                        <a href='#'>Brokerage charges</a> 
+                        <a href='#'>Market holidays</a>
+                        <a href='#'>Economic calendar</a>
+                        <a href='#'>Calculators</a> 
+                        <a href='#'>Markets</a> 
+                        <a href='#'>Sectors</a> 
                     </div>
                     <div className='footer-col'>
                         <p>Support</p>
-                        <a href=''>Contact us</a> 
-                        <a href=''>Support portal</a> 
-                        <a href=''>How to file a complaint?</a>
-                        <a href=''>Status of your complaints</a>
-                        <a href=''>Bulletin</a> 
-                        <a href=''>Circular</a> 
-                        <a href=''>Z-Connect blog</a>  
-                        <a href=''>Downloads</a>  
+                        <a href='#'>Contact us</a> 
+                        <a href='#'>Support portal</a> 
+                        <a href='#'>How to file a complaint?</a>
+                        <a href='#'>Status of your complaints</a>
+                        <a href='#'>Bulletin</a> 
+                        <a href='#'>Circular</a> 
+                        <a href='#'>Z-Connect blog</a>  
+                        <a href='#'>Downloads</a>  
                     </div>
                     <div className='footer-col'>
                         <p>Account</p>
-                        <a href=''>Contact us</a> 
-                        <a href=''>Open demat account</a> 
-                        <a href=''>Minor demat account</a>
-                        <a href=''>60 days challenge</a>
+                        <a href='#'>Contact us</a> 
+                        <a href='#'>Open demat account</a> 
+                        <a href='#'>Minor demat account</a>
+                        <a href='#'>60 days challenge</a>
                     </div>
                 </div>
                 
@@ -54,13 +54,13 @@ function Footer() {
                     
                     <p>Procedure to file a complaint on SEBI SCORES: Register on SCORES portal. Mandatory details for filing complaints on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits: Effective Communication, Speedy redressal of the grievances</p>
                     
-                    <a href=''>Smart Online Dispute Resolution | Grievances Redressal Mechanism</a>
+                    <a href='#'>Smart Online Dispute Resolution | Grievances Redressal Mechanism</a>
 
                     <p>Investments in securities market are subject to market risks; read all the related documents carefully before investing.</p>
 
                     <p>Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month.</p>
 
-                    <p>India's largest broker based on networth as per NSE. <a href=''>NSE broker factsheet</a></p>
+                    <p>India's largest broker based on networth as per NSE. <a href='#'>NSE broker factsheet</a></p>
 
                     <p>"Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please create a ticket here.</p>
                 </div>
--- frontend/src/landing_page/user/Login.js
+++ frontend/src/landing_page/user/Login.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { API_URL } from '../../config';
 
 const Login = ({ onLoginSuccess }) => {
-  const navigate = useNavigate();
   const [inputValue, setInputValue] = useState({
     username: "",
     password: "",
