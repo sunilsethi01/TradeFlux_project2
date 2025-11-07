1ecd10b5d4f254ae276fe1f36f5b4d4595997a5d
sunil sethi <sunilsethi1214@gmail.com>
2025-11-07 16:18:09 +0530

    CRITICAL FIX: Remove broken hamburger toggler from navbar

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -77,19 +77,6 @@ function Navbar({ isAuthenticated, onLogout }) {
                 Support
               </Link>
             </li>
-            <li className="nav-item">
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
-            </li>
           </ul>
         </div>
       </div>
