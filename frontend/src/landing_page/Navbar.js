b085b3bb25bb28671d5722341e4a2064076afa3d
sunil sethi <sunilsethi1214@gmail.com>
2025-11-07 16:52:59 +0530

    Fix hamburger menu position - now visible on mobile, working on all screens

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -23,6 +23,17 @@ function Navbar({ isAuthenticated, onLogout }) {
             alt="Trade Flux Logo"
           />
         </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto">
             {!isAuthenticated && (
@@ -76,20 +87,6 @@ function Navbar({ isAuthenticated, onLogout }) {
               <Link className="nav-link" to="/support">
                 Support
               </Link>
-            </li>
-            <li className="nav-item">
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-                style={{border: 'none', background: 'none'}}
-              >
-                <span className="navbar-toggler-icon"></span>
-              </button>
             </li>
           </ul>
         </div>
